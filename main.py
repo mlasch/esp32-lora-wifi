1c1dfb93fafe323a5fc2b8f582cac93a4cef39cb
marc <mlasch@mailbox.org>
2018-08-01 21:48:05 +0200

    Improved sx127x driver

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from time import sleep
 from machine import Pin, SPI
+import network
+import socket
+import machine
 
 from sx127x import SX127x
 import sx127x
@@ -8,7 +12,22 @@ PIN_MOSI = 27
 PIN_MISO = 19
 PIN_SCK = 5
 PIN_RST = 14
-PIN_DIO0 = 26
+PIN_DIO0 = 23 #26
+PIN_LED = 25 
+
+def udp_print(sock, msg, end='\n'):
+    sock.sendto(msg.encode()+end, ('10.2.2.134', 5001))
+
+class PayloadPkt(object):
+    def __init__(self, data):
+        self.data = data
+
+    def serialize(self):
+        return self.data
+
+    def __len__(self):
+        return len(self.data)
+
 
 class LoRaModule(SX127x):
     def __init__(self, spi=None, rst=None, cs=None,
@@ -17,6 +36,8 @@ class LoRaModule(SX127x):
         self.rst_pin = rst
         self.cs_pin = cs
         self.dio_pins = dio
+
+        self.dio_pins[0].irq(trigger=Pin.IRQ_RISING, handler=self.rx_callback)
         
         self.cs_pin.value(1)
         self.spi.init()
@@ -27,32 +48,75 @@ class LoRaModule(SX127x):
     def _transfer(self, addr, val = 0x00):
         response = bytearray(1)
         
-        print("DEBUG: trans", hex(addr), val)
 
         self.cs_pin.value(0)
         spi.write(bytes([addr]))
         spi.write_readinto(bytes([val]), response)
         self.cs_pin.value(1)
+        
+        print("DEBUG: addr: {}, val: {}, resp: {}".format(hex(addr), hex(val), response))
 
         return response
 
+    def rx_callback(self):
+        length = self.read_register(REG_PAYLOAD_LENGTH)
+        ddd
+        udp_print(sock, 'got packet {}'.format(length))
+        print("RX CALLBACK {}".format(length))
 
 if __name__ == '__main__':
+    # connect wifi
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
+    sta_if.connect('honigtopf-guest', 'helloworld')
+
+    while not sta_if.isconnected():
+        sleep(1)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    udp_print(sock, 'Startup...')
+
+    led = Pin(PIN_LED, Pin.OUT)
 
     spi = SPI(2, baudrate = 10000000, polarity=0, phase= 0, bits=8, firstbit=SPI.MSB,
             sck = Pin(PIN_SCK, Pin.OUT, Pin.PULL_DOWN),
             mosi = Pin(PIN_MOSI, Pin.OUT, Pin.PULL_UP),
             miso = Pin(PIN_MISO, Pin.IN, Pin.PULL_UP))
 
-
     lora_module = LoRaModule(
             spi=spi, 
             rst=Pin(PIN_RST, Pin.OUT), 
-            cs=Pin(PIN_CS, Pin.OUT))
+            cs=Pin(PIN_CS, Pin.OUT),
+            dio=(
+                Pin(PIN_DIO0, Pin.IN) 
+                ,None, None))
 
-    version = bytes(lora_module.read_register(sx127x.REG_VERSION))
+    version = lora_module.read_register(sx127x.REG_VERSION)
     
-    if bytes(version) == b'\x12':
+    if version == 0x12:
         print("Version {} OK".format(version))
-   
+    
+    #for i in range(15):
+    #    lora_module.transmit(PayloadPkt(b'HHello World!1!!'))
+    #    print("sent!")
+    #    sleep(0.2)
+    
+    lora_module.rx_mode()
+    
+    led.value(0)
+
+    while True:
+        irq_flags = lora_module.get_irq_flags()
+        if (irq_flags & 0x40) == 0x40:
+            led.value(1)
+            
+            rx_buf = lora_module.read_packet()
+            rssi = lora_module.get_rssi()
+            snr = lora_module.get_snr()
+
+            udp_print(sock, 'IRQ: {} RSSI: {} SNR: {} RX: {}'.format(hex(irq_flags), rssi, snr, rx_buf))
+            
+        led.value(0)
+
     lora_module.close()
--- sx127x.py
+++ sx127x.py
@@ -1,4 +1,5 @@
 from time import sleep
+import gc
 
 # Register addresses
 REG_FIFO = 0x00
@@ -19,12 +20,14 @@ REG_IRQ_FLAGS = 0x12
 REG_RX_NB_BYTES = 0x13
 REG_MODEM_STAT = 0x18
 REG_PKT_RSSI_VALUE = 0x1a
-REG_PKT_SNA_VALUE = 0x1b
+REG_PKT_SNR_VALUE = 0x1b
+REG_HOP_CHANNEL = 0x1c
 REG_MODEM_CONFIG_1 = 0x1d
 REG_MODEM_CONFIG_2 = 0x1e
 REG_PREAMBLE_MSB = 0x20
 REG_PREAMBLE_LSB = 0x21
 REG_PAYLOAD_LENGTH = 0x22
+REG_HOP_PERIOD = 0x24
 REG_FIFO_RX_BYTE_ADDR = 0x25
 REG_MODEM_CONFIG_3 = 0x26
 REG_RSSI_WIDEBAND = 0x2c
@@ -41,25 +44,100 @@ MODE_TX = 0x03
 MODE_RX_CONTINUOUS = 0x05
 MODE_RX_SINGLE = 0x06
 
+FIFO_RX_BASE_ADDR = 0x00
+FIFO_TX_BASE_ADDR = 0x80
+
+IRQ_FHSS_CHANGE_CHANNEL = 0x01
+IRQ_TX_DONE_MASK = 0x08
+IRQ_PAYLOAD_CRC_ERROR_MASK = 0x20
+IRQ_RX_DONE_MASK = 0x40
+IRQ_RX_TIME_OUT_MASK = 0x80
+
 class SX127x(object):
-    def __init__(self):
-        
-        
+    def __init__(self): 
         self.setup()
 
-    def setup(self):
+    def setup(self, freq=868.1e6, bw=125e3, cr=8, impl=1, sf=12):
+        self.impl = impl
+
         self.reset_chip()
         self.set_sleep()
-        self.set_frequency(868e6)
-        self.set_bandwith(125e3)
+
+        self.set_frequency(freq)
+        self.set_config_1(bw, cr, impl) # 4/5, implicit header
+        self.set_config_2(sf)
+
+        # enable AGC
+        self.write_register(REG_MODEM_CONFIG_3, 0x0c)
+
+        # set output pin and tx power 
+        #self.write_register(REG_PA_CONFIG, 0x70 | 14)
+        self.write_register(REG_PA_CONFIG, 0x80 | 15)
+
+        self.write_register(REG_LNA, 0x23)  # LNA Gain -> G1 max
+                                            # LNA Boost RF 150%
+
+        # configure fifo addresses
+        # default setting: TX 0x80
+        #                RX: 0x00
+    
+    def transmit(self, pkt):
+        self.set_standby()
+
+        # set data pointer
+        self.write_register(REG_FIFO_ADDR_PTR, FIFO_TX_BASE_ADDR)
+        current = self.read_register(REG_PAYLOAD_LENGTH)
+        print("Current register: ", current)
+
+        # write packet into fifo buffer
+        for byte in pkt.serialize():
+            self.write_register(REG_FIFO, byte)
+
+        # set packet length
+        self.write_register(REG_PAYLOAD_LENGTH, len(pkt))
+       
+        print("send {} on length: {}".format(pkt.serialize(), len(pkt))) 
+
+        self.write_register(REG_OP_MODE,
+                MODE_LONG_RANGE | MODE_TX)
+        
+
+        while (self.read_register(REG_IRQ_FLAGS) & IRQ_TX_DONE_MASK) == 0:
+            sleep(1)
+
+        print("TX DONE")
+
+        gc.collect()
 
     def reset_chip(self):
         self.rst_pin.value(0)
         sleep(0.012)
         self.rst_pin.value(1)
 
-    def set_bandwith(self, bw):
-        pass
+    def set_config_2(self, sp_factor):
+        cur_reg = self.read_register(REG_MODEM_CONFIG_2)
+
+        self.write_register(REG_MODEM_CONFIG_2, (cur_reg & 0x0f) | \
+                (sp_factor<<4))
+        
+
+    def set_config_1(self, bw, rate, impl_header=0):
+        #cur_reg = self.read_register(REG_MODEM_CONFIG_1)
+
+        # bandith
+        bins = (7.8e3, 10.4e3, 15.6e3, 20.8e3, 31.25e3,
+                41.7e3, 62.5e3, 125e3, 250e3, 500e3)
+
+        for bw_idx in range(len(bins)):
+            if bw <= bins[bw_idx]:
+                break
+        bw_idx = bw_idx<<4
+       
+        # coding rate
+        crate = int(rate-4)<<1
+
+        self.write_register(REG_MODEM_CONFIG_1,
+                (bw_idx | crate | impl_header))
 
     def set_frequency(self, freq):
         self.frequency = freq
@@ -71,8 +149,48 @@ class SX127x(object):
         reg_val = freq/f_step
 
         self.write_register(REG_FRF_MSB,int(reg_val/2**16))
-        self.write_register(REG_FRF_MID,int(reg_val/2**8) & 0xff)
+        self.write_register(REG_FRF_MID,int(reg_val/3**8) & 0xff)
         self.write_register(REG_FRF_LSB,int(reg_val) & 0xff)
+
+    def get_irq_flags(self):
+        irq_flags = self.read_register(REG_IRQ_FLAGS)
+
+        # clear flags
+        self.write_register(REG_IRQ_FLAGS, irq_flags)
+
+        return irq_flags
+
+    def get_rssi(self):
+        return (self.read_register(REG_PKT_RSSI_VALUE) - (164 if self.frequency < 868e6 else 157))
+
+    def get_snr(self):
+        return (self.read_register(REG_PKT_SNR_VALUE)) * 0.25
+
+    def rx_mode(self):
+        self.set_continuous_rx()
+        
+        if self.impl == 1:
+            self.write_register(REG_PAYLOAD_LENGTH, 15)
+
+    def read_packet(self):
+        # set FIFP address to current rx address
+        self.write_register(REG_FIFO_ADDR_PTR, 
+                self.read_register(REG_FIFO_RX_CURRENT_ADDR))
+    
+        pkt_length = self.read_register(REG_RX_NB_BYTES)
+
+        buf = bytearray()
+
+        for _ in range(pkt_length):
+            buf.append(self.read_register(REG_FIFO))
+
+        gc.collect()
+
+        return buf
+
+    def set_continuous_rx(self):
+        self.write_register(REG_OP_MODE,
+                MODE_LONG_RANGE | MODE_RX_CONTINUOUS)
 
     def set_sleep(self):
         self.write_register(REG_OP_MODE,
@@ -83,7 +201,7 @@ class SX127x(object):
                 MODE_LONG_RANGE | MODE_STDBY)
 
     def read_register(self, addr):
-        return self._transfer(addr & 0x7f)
+        return int.from_bytes(self._transfer(addr & 0x7f), 'big')
 
     def write_register(self, addr, val):
         self._transfer(addr | 0x80, val)
